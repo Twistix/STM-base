1881582de25f9fd182fad81d4ea558b31ae57060
Julien Armengaud <julien.armengaud@viveris.fr>
2025-02-17 15:03:51 +0100

    [UPDATE] Add some source files to the project files rather than in submodule

--- src/main.c
+++ src/main.c
@@ -48,35 +48,52 @@ int main(void)
     /* =============================================================== */
 
     // Main loop
-    while (1) {}
+    while (1) {
+#if 0
+        uint8_t test_buffer[10] = {1,2,3,4,5,6,7,8,9,10};
+
+        // Set CS low
+        HAL_GPIO_WritePin(SD_CS_GPIO_Port, SD_CS_Pin, GPIO_PIN_RESET);
+
+        // Write SPI
+        //HAL_SPI_Transmit(&SD_SPI_HANDLE, test_buffer, 10, 0);
+
+        HAL_Delay(10);
+
+        // Set CS high
+        HAL_GPIO_WritePin(SD_CS_GPIO_Port, SD_CS_Pin, GPIO_PIN_SET);
+
+        // Delay
+        HAL_Delay(100);
+#endif
+    }
 }
 
 
 void SystemClock_Config(void) {
-    RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
     RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+    RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-    /* Enable HSE Oscillator and Activate PLL with HSE as source */
-    RCC_OscInitStruct.OscillatorType      = RCC_OSCILLATORTYPE_HSI;
-    RCC_OscInitStruct.HSIState            = RCC_HSI_ON;
+    /** Configure the main internal regulator output voltage
+     */
+    __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+    /** Initializes the RCC Oscillators according to the specified parameters
+     * in the RCC_OscInitTypeDef structure.
+     */
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+    RCC_OscInitStruct.HSIState = RCC_HSI_ON;
     RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-    RCC_OscInitStruct.PLL.PLLState        = RCC_PLL_ON;
-    RCC_OscInitStruct.PLL.PLLSource       = RCC_PLLSOURCE_HSI;
-    RCC_OscInitStruct.PLL.PLLMUL          = RCC_PLL_MUL6;
-    RCC_OscInitStruct.PLL.PLLDIV          = RCC_PLL_DIV3;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+    RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL4;
+    RCC_OscInitStruct.PLL.PLLDIV = RCC_PLL_DIV2;
     HAL_RCC_OscConfig(&RCC_OscInitStruct);
 
-
-    /* Set Voltage scale1 as MCU will run at 32MHz */
-    __HAL_RCC_PWR_CLK_ENABLE();
-    __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
-    
-    /* Poll VOSF bit of in PWR_CSR. Wait until it is reset to 0 */
-    while (__HAL_PWR_GET_FLAG(PWR_FLAG_VOS) != RESET) {};
-
-    /* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2
-    clocks dividers */
-    RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
+    /** Initializes the CPU, AHB and APB buses clocks
+     */
+    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                                |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
     RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
     RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
     RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
@@ -90,6 +107,14 @@ void MX_GPIO_Init(void) {
 
     /* Enable GPIO clock */
     __HAL_RCC_GPIOA_CLK_ENABLE();
+    __HAL_RCC_GPIOB_CLK_ENABLE();
+
+    // Configure SPI1 SCK (PB3), MISO (PB4), and MOSI (PB5)
+    GPIO_InitStruct.Pin = GPIO_PIN_3 | GPIO_PIN_4 | GPIO_PIN_5;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;  // Push-pull mode for SPI
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+    GPIO_InitStruct.Pull = GPIO_PULLUP;
+    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
     /* Configure SD CS pin */
     GPIO_InitStruct.Pin = SD_CS_Pin;
@@ -102,16 +127,20 @@ void MX_GPIO_Init(void) {
 
 
 void MX_SPI1_Init(void) {
-    SD_SPI_HANDLE.Instance = SD_SPI_BUS;
-    SD_SPI_HANDLE.Init.Mode = SPI_MODE_MASTER;  // Master mode
-    SD_SPI_HANDLE.Init.Direction = SPI_DIRECTION_2LINES;  // Full-duplex
-    SD_SPI_HANDLE.Init.DataSize = SPI_DATASIZE_8BIT;  // 8-bit data
-    SD_SPI_HANDLE.Init.CLKPolarity = SPI_POLARITY_LOW;  // CPOL = 0
-    SD_SPI_HANDLE.Init.CLKPhase = SPI_PHASE_1EDGE;  // CPHA = 0
-    SD_SPI_HANDLE.Init.NSS = SPI_NSS_SOFT;  // Software NSS (manual CS control)
-    SD_SPI_HANDLE.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_64;  // ~400kHz (low-speed init)
-    SD_SPI_HANDLE.Init.FirstBit = SPI_FIRSTBIT_MSB;  // MSB first
+    /* Enable SPI1 clock */
+    __HAL_RCC_SPI1_CLK_ENABLE();
+
+    SD_SPI_HANDLE.Instance = SPI1;
+    SD_SPI_HANDLE.Init.Mode = SPI_MODE_MASTER;
+    SD_SPI_HANDLE.Init.Direction = SPI_DIRECTION_2LINES;
+    SD_SPI_HANDLE.Init.DataSize = SPI_DATASIZE_8BIT;
+    SD_SPI_HANDLE.Init.CLKPolarity = SPI_POLARITY_LOW;
+    SD_SPI_HANDLE.Init.CLKPhase = SPI_PHASE_1EDGE;
+    SD_SPI_HANDLE.Init.NSS = SPI_NSS_SOFT;
+    SD_SPI_HANDLE.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_2;
+    SD_SPI_HANDLE.Init.FirstBit = SPI_FIRSTBIT_MSB;
     SD_SPI_HANDLE.Init.TIMode = SPI_TIMODE_DISABLE;
     SD_SPI_HANDLE.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
+    SD_SPI_HANDLE.Init.CRCPolynomial = 10;
     HAL_SPI_Init(&SD_SPI_HANDLE);
 }
